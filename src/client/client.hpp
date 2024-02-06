04242cc929ccee7be5a1c35436997b2f459afd24
Christopher Liu <chrisckeyliu@gmail.com>
2024-02-06 21:01:11 +0800

    [Upd] Add FPS

--- src/client/client.hpp
+++ src/client/client.hpp
@@ -1,6 +1,7 @@
 #ifndef __CLIENT_HPP__
 #define __CLIENT_HPP__
 
+#include <chrono>
 #include <mqtt/async_client.h>
 #include <mqtt/callback.h>
 #include <mqtt/connect_options.h>
@@ -16,6 +17,39 @@
 #include "mqtt/mqtt.hpp"
 
 extern std::shared_ptr<spdlog::logger> logger;
+
+using namespace std;
+using namespace std::chrono;
+class FPSCounter {
+
+private:
+  high_resolution_clock::time_point lastTime;
+  int frameCount;
+  double fps;
+
+public:
+  FPSCounter() : frameCount(0), fps(0.0) {
+    lastTime = high_resolution_clock::now();
+  }
+
+  // 更新帧计数器并计算FPS（如果适用）
+  double update() {
+    ++frameCount;
+
+    auto currentTime = high_resolution_clock::now();
+    double elapsedTime =
+        duration_cast<duration<double>>(currentTime - lastTime).count();
+
+    // 如果超过1秒，则计算FPS
+    if (elapsedTime >= 1.0) {
+      fps = frameCount / elapsedTime;
+      frameCount = 0;
+      lastTime = currentTime;
+    }
+
+    return fps; // 返回最新的FPS值（如果还没更新则返回上一秒的FPS）
+  }
+};
 
 class Client {
 
@@ -49,10 +83,13 @@ private:
            }},
   };
 
+  FPSCounter fps_;
+
 public:
   std::function<void(const mqtt::const_message_ptr msg)> GetHandler();
   Client();
   Client(std::string addr, std::string id, std::string name);
+
   void start() {
 
     mqtt_->connect();
@@ -63,8 +100,9 @@ public:
 
     logger->info("Setting Routine, Publishing image data via `/data`");
     loop_.routine([=]() {
+      logger->info("Fps: {}", fps_.update());
       rgb_ = camera_->GetImageMqtt(); // Last two Bytes are Height and Width
-      logger->trace("Publishing Data through `/data`");
+      logger->debug("Publishing Data through `/data`");
       mqtt_->publish("/data", std::string(rgb_.begin(), rgb_.end()));
       logger->trace("`/data` Published");
     });
